70317e47828ea47414a0518efaf7267cbce9a39f
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-20 16:59:57 -0700

    set up ArtistDetail, moving to services

--- src/containers/ArtistDetail.js
+++ src/containers/ArtistDetail.js
@@ -1,1 +1,40 @@
 import React, { PureComponent } from 'react';
+import Paging from '../components/Paging';
+import Releases from '../components/detail/Releases';
+import theBeatles from '../../data/the-beatles';
+
+export default class ArtistDetail extends PureComponent {
+  state = {
+    currentPage: 1,
+    totalPages: 10,
+    artist: theBeatles
+  }
+
+  handleNextButton = () => {
+    this.setState(state => {
+      return {
+        currentPage: state.currentPage + 1
+      };
+    });
+  }
+
+  handlePreviousButton = () => {
+    this.setState(state => {
+      return {
+        currentPage: state.currentPage - 1
+      };
+    });
+  }
+
+  render() {
+    const { currentPage, totalPages, artist } = this.state;
+    const { handleNextButton, handlePreviousButton } = this;
+    const pagingProps = { currentPage, totalPages, handleNextButton, handlePreviousButton };
+    return (
+      <>
+        <Paging { ...pagingProps} />
+        <Releases artist={artist} />
+      </>
+    );
+  }
+}
