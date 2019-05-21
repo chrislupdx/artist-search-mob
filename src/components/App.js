a10c66c4d09043f9f446b0cba0e84c902cea1b1f
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-21 13:31:44 -0700

    lyrics display properly

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ArtistSearch from '../containers/ArtistSearch';
 import ArtistDetail from '../containers/ArtistDetail';
 import ReleaseDetail from '../containers/ReleaseDetail';
+import LyricDisplay from '../containers/LyricDisplay';
 
 export default function App() {
   
   return (
     <Router>
       <Switch>
+        <Route path ="/song/:title/:name" component={LyricDisplay} />
         <Route path="/release/:id/:title/:name" component={ReleaseDetail} />
         <Route path="/artist/:id" component={ArtistDetail} />
         <Route path="/" component={ArtistSearch} />
--- src/containers/LyricDisplay.js
+++ src/containers/LyricDisplay.js
@@ -1,1 +1,24 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import lyricsData from '../../data/lyricsData';
+export default class LyricDisplay extends PureComponent {
+  static propTypes = {
+    match: PropTypes.object.isRequired
+  }
+
+  render() {
+    const artistName = decodeURIComponent(this.props.match.params.name);
+    const title = decodeURIComponent(this.props.match.params.title);
+    const lyrics = lyricsData.lyrics;
+
+    return (
+      <>
+      <h2>{artistName}</h2>
+      <h3>{title}</h3>
+      <p>{lyrics}</p>
+      </>
+
+    );
+
+  }
+}
